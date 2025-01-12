59123b0e1bf336535bc63ff76c4ddef5feb2fd20
RAVI KUMAR <ravikumartakajm@gmail.com>
2025-01-12 19:07:12 +0530

    debug the vercel environments

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+// export const baseURL =
+//   process.env.VERCEL_ENV === 'production'
+//     ? `https://${process.env.VERCEL_URL || 'https://tree-view-itrofc8iu-ravi-taks-projects.vercel.app'}`
+//     : 'http://localhost:3000'
+
 export const baseURL =
-  process.env.VERCEL_ENV === 'production'
-    ? `https://${process.env.VERCEL_URL || 'https://tree-view-itrofc8iu-ravi-taks-projects.vercel.app'}`
-    : 'http://localhost:3000'
+  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000'
